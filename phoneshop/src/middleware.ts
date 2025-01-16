cbc55c9650695dd1995999ba526501014996cae3
We5t <vitaliy14042007@gmail.com>
2025-01-16 12:11:14 +0100

    Add: Middleware + AuthConfig

---
+++ phoneshop/src/middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+})
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
