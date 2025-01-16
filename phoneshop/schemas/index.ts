9e16feffde5bc5a55fc1c16a8066ceedfe8f8149
We5t <vitaliy14042007@gmail.com>
2025-01-16 11:33:11 +0100

    Add: Registration + Users DB

---
+++ phoneshop/schemas/index.ts
@@ -0,0 +1,34 @@
+import * as z from "zod";
+
+export const SettingsSchema = z.object({
+    name: z.optional(z.string()),
+    image: z.optional(z.string()),
+    banner: z.optional(z.string()),
+});
+
+export const LoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1 ,{
+        message: "Password is required"
+    }),
+});
+
+export const RegisterSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6 ,{
+        message: "Minimum 6 characters required"
+    }),
+    name: z.string().min(1, {
+        message: "Name is required",
+    }),
+});
+
+export const ResetSchema = z.object({
+    email: z.string().email(),
+    });
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+         message: "Minimum 6 characters required"
+    }),
+});
